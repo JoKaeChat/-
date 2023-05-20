16e40c9993c685563f4f9be2a731db33789cb99b
ajou20658 <kwy1379@ajou.ac.kr>
2023-05-20 22:08:00 +0900

    profile 세션으로 내 정보 받아오기

--- src/pages/member/profile.js
+++ src/pages/member/profile.js
@@ -4,22 +4,32 @@ import { useState, useEffect } from "react";
 
 function WikiDocHistory() {
   const [user, setUser] = useState("");
+
   useEffect(() => {
-    fetch("http://localhost:3001/api/user", {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setUser(data);
-      })
-      .catch((error) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/user", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data);
+          //console.log(data);
+        } else {
+          throw new Error("Request failed with status " + response.status);
+        }
+      } catch (error) {
         console.error(error);
-      });
+      }
+    };
+
+    fetchData();
   }, []);
+  console.log(user[0]);
   return (
     <MainLayout>
-      <div>{user}</div>
+      <h1>{user[0].userID}</h1>
     </MainLayout>
   );
 }
