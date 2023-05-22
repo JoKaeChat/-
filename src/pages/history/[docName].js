a81993d8ad96d4a91ecbadf6d322ffb3432e727a
ajou20658 <kwy1379@ajou.ac.kr>
2023-05-22 17:56:41 +0900

    문서 역사 리스트 저장

--- src/pages/history/[docName].js
+++ src/pages/history/[docName].js
@@ -2,43 +2,80 @@ import MainLayout from "../../components/mainLayout";
 import s from "../../styles/Wikidoc.module.css";
 import { useNavigate } from "react-router";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+function WikiDocHistory() {
+  const { title } = useParams();
+  const [list, setList] = useState([]);
 
-function WikiDocHistory(){
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3001/api/history/${title}`,
+          {
+            method: "GET",
+            credential: "include",
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          //console.log(data);
 
-    const {title} = useParams();
-   
-
-    return(
-       
-        <MainLayout>
-        <div>
+          setList([...list, data]);
+        } else {
+          console.log("문서만들기 버튼 보이게 구현");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPost();
+  }, [title]);
+  if (!list) {
+    return (
+      <div>
+        <h2>이 문서는 편집 역사가 없습니다.</h2>
+      </div>
+    );
+  }
+  console.log(list);
+  return (
+    <MainLayout>
+      <div>
         <div className={s.header}>
           <div className={s.headerContainer}>
-                <h1 className={s.title}>[호랑이] (문서 역사)</h1>
-            
-            <div>
-                <div className={s.buttonContainer}>
-                    <a href= {'/edit/' + {title}.title} id={s.buttonFirst}> 편집 </a>
-                    <a>토론</a>
-                    <a href ={'/history/'+{title}.title}id={s.buttonLast}>문서 역사</a>
+            <h1 className={s.title}>[{title}] (문서 역사)</h1>
 
-                </div>
-                <div className={s.time}>최근 수정 시각</div>
+            <div>
+              <div className={s.buttonContainer}>
+                <a href={"/edit/" + { title }.title} id={s.buttonFirst}>
+                  {" "}
+                  편집{" "}
+                </a>
+                <a>토론</a>
+                <a href={"/history/" + { title }.title} id={s.buttonLast}>
+                  문서 역사
+                </a>
+              </div>
+              <div className={s.time}>최근 수정 시각</div>
             </div>
           </div>
         </div>
-            
-            <ul className={s.historyList}>
-                <li>[문서 생성 시간][버전 이름] <a>보기</a></li>
-                <li>[문서 생성 시간][버전 이름] <a>보기</a></li>
-                <li>[문서 생성 시간][버전 이름] <a>보기</a></li>
-            </ul>
-        
-        
-        </div>
-        </MainLayout>
 
-    )
+        <ul className={s.historyList}>
+          <li>
+            [문서 생성 시간][버전 이름] <a>보기</a>
+          </li>
+          <li>
+            [문서 생성 시간][버전 이름] <a>보기</a>
+          </li>
+          <li>
+            [문서 생성 시간][버전 이름] <a>보기</a>
+          </li>
+        </ul>
+      </div>
+    </MainLayout>
+  );
 }
 
 export default WikiDocHistory;
