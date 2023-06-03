7df132c243a0d1b186b8ea5cc290562f5defb37e
ajou20658 <kwy1379@ajou.ac.kr>
2023-06-03 17:25:18 +0900

    hi

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const [close, setClose] = useState(false);
   const [text, setText] = useState("");
   const movePage = useNavigate();
 
   const clickUser = (e) => {
     e.preventDefault();
+    setClose((current) => !current);
   };
 
   const handleChange = (e) => {
@@ -19,23 +21,31 @@ function Header() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (text === "") {
+      return;
+    }
     movePage(`/w/${text}`);
+    setText("");
   };
 
   const handleClick = (e) => {
     e.preventDefault();
+    if (text === "") {
+      return;
+    }
     movePage(`/w/${text}`);
+    setText("");
   };
 
   return (
     <header>
       <div className={s.header}>
         <nav className={s.headerContainer}>
-          <div>
+          <div className={s.headerListContainer}>
             <ul>
               <li>
                 {" "}
-                <a href="/">멋사위키</a>
+                <Link to={"/"}>멋사위키</Link>
               </li>
               <li> 메뉴 1</li>
               <li> 메뉴 2</li>
@@ -45,6 +55,7 @@ function Header() {
           <div className={s.searchBarContainer}>
             <form className={s.searchBar} onSubmit={handleSubmit}>
               <input
+                value={text}
                 type="text"
                 placeholder="Search"
                 onChange={handleChange}
@@ -59,11 +70,13 @@ function Header() {
             <button className={s.user} onClick={clickUser}>
               <FontAwesomeIcon icon={faUser} />
             </button>
-            <div className={`${s.pop} `}>
-              <a href="/member/profile">내 정보</a>
-              <a href="#">내 문서 기여 목록</a>
-              <a href="/member/login">로그인</a>
-            </div>
+            {close && (
+              <div className={s.pop}>
+                <Link to={"/member/mypage"}>내 정보</Link>
+
+                <Link to={"/member/login"}>로그인</Link>
+              </div>
+            )}
           </div>
         </nav>
       </div>
